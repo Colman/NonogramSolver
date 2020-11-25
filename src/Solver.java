f21bdf556f15726634f574990e9e63b74e8d0f52
Colman Koivisto <colmankoivisto@gmail.com>
2020-11-24 17:48:25 -0700

    Second Sudoku

--- src/Solver.java
+++ src/Solver.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import game.Game;
+import util.AI;
+import util.Context;
+import util.Move;
 
-public class Solver {
+public class Solver extends AI {
+    /**
+     * Constructor
+     */
+    public Solver() {
+        this.friendlyName = "SudokuSolver";
+    }
+
+    //-------------------------------------------------------------------------
+
+    @Override
+    public Move selectAction(Game game, Context context, double maxSeconds, int maxIterations, int maxDepth) {
+
+        return null;
+    }
+
+    @Override
+    public void initAI(Game game, int playerID) {
+    }
 }
