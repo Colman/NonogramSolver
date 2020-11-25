0965603e780f1c556e877cf5a0dfaa4fc43bc903
Colman Koivisto <colmankoivisto@gmail.com>
2020-11-25 12:10:55 -0700

    Finding values

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import game.Game;
+import util.AI;
+import util.Context;
+import util.GameLoader;
+import util.Trial;
+import util.model.Model;
 
+import java.util.ArrayList;
 
 public class Main {
+    static final String GAME_NAME = "Sudoku.lud";
 
     public static void main(String[] args) {
+        Game game = GameLoader.loadGameFromName(GAME_NAME);
 
+        final Trial trial = new Trial(game);
+        final Context context = new Context(game, trial);
+
+        final ArrayList<AI> ais = new ArrayList<AI>();
+        ais.add(null);
+        ais.add(new Solver());
+
+        game.start(context);
+
+        ais.get(1).initAI(game, 1);
+
+        Model model = context.model();
+        while (!context.trial().over()) {
+            model.startNewStep(context, ais, 1.0);
+        }
+
+        System.out.println("Outcome = " + context.trial().status());
     }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,12 +1,12 @@
 import game.Game;
+import game.types.board.SiteType;
+import game.util.graph.Vertex;
 import util.AI;
 import util.Context;
 import util.Move;
+import util.action.Action;
 
 public class Solver extends AI {
-    /**
-     * Constructor
-     */
     public Solver() {
         this.friendlyName = "SudokuSolver";
     }
@@ -16,10 +16,15 @@ public class Solver extends AI {
     @Override
     public Move selectAction(Game game, Context context, double maxSeconds, int maxIterations, int maxDepth) {
 
+        for (Vertex vertex : game.board().graph().vertices()) {
+           // System.out.println(vertex.id());
+        }
+
         return null;
     }
 
     @Override
     public void initAI(Game game, int playerID) {
+        //Do nothing
     }
 }
