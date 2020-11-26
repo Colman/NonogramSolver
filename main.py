50a30a95cf038406ab4d889938eaf2825d950721
Duncan Krammer <duncankrammer@gmail.com>
2020-11-26 13:59:38 -0700

    Basic candidate checker, as a basis for Crook's

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from board import Board
 from brute import Brute
 import time
+import candidates
 
 
 board = Board()
 board.parse_board("boards/hard.txt")
 
+'''
 brute = Brute(board)
 start = time.time()
 brute.solve()
@@ -13,4 +15,18 @@ end = time.time()
 
 print("")
 print("Solved in: {:f} seconds".format(end - start))
+'''
 board.print()
+
+
+candidates = candidates.exhaust_candidates(board)
+print(candidates)
+board.print()
+
+'''
+for k, v in cand.items():
+    if k in new_cand:
+        if cand[k] != new_cand[k]:
+            print("old vals: ", k, cand[k])
+            print("new vals: ", k, new_cand[k])
+'''
