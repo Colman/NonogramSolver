14637066962d33f6b30802d2dde8699f903e7ad5
Duncan Krammer <duncankrammer@gmail.com>
2020-11-26 23:00:45 -0700

    Fixed bug where inv mv2 would exit program

--- cli.py
+++ cli.py
@@ -2,46 +2,51 @@ import time
 
 
 class Cli:
-	def __init__(self, board, solver):
-		self.board = board
-		self.solver = solver
+        def __init__(self, board, solver):
+                self.board = board
+                self.solver = solver
 
 
-	def start(self):
-		while True:
-			cmd = input("Please enter a move (ex. B3 2), \"solve\", or \"show\": ")
-			if cmd == "solve":
-				start = time.time()
-				try:
-					self.solver.solve()
-				except:
-					print("Unsolvable from here")
-					continue
+        def start(self):
+                while True:
+                        cmd = input("Please enter a move (ex. B3 2), \"solve\", \"show\", or \"end\": ")
+                        if cmd == "solve":
+                                start = time.time()
+                                try:
+                                        self.solver.solve()
+                                except:
+                                        print("Unsolvable from here")
+                                        continue
 
-				end = time.time()
-				print("")
-				print("Solved in: {:f} seconds".format(end - start))
-				self.board.print()
-				break
+                                end = time.time()
+                                print("")
+                                print("Solved in: {:f} seconds".format(end - start))
+                                self.board.print()
+                                break
 
-			elif cmd == "show":
-				self.board.print()
+                        elif cmd == "show":
+                                self.board.print()
 
-			else:
-				cmds = cmd.split(" ")
-				if len(cmds) == 2:
-					index = self.board.get_index_from_label(cmds[0])
-					if index == -1:
-						print("Invalid move1")
+                        elif cmd == "end":
+                                print("Bye!")
+                                break
 
-					else:
-						try:
-							value = int(cmds[1])
-							self.board.play_move(index, value)
-						except:
-							print("Invalid move2")
-							return
+                        else:
+                                cmds = cmd.split(" ")
+                                if len(cmds) == 2:
+                                        index = self.board.get_index_from_label(cmds[0])
+                                        if index == -1:
+                                                print("Invalid move1")
 
-				else:
-					print("Invalid move3")
+                                        else:
+                                                try:
+                                                        value = int(cmds[1])
+                                                        self.board.play_move(index, value)
+                                                except:
+                                                        print("Invalid move2")
+                                                        #return
+
+                                else:
+                                        print("Invalid move3")
+
 
